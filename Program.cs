13669137d55c5a5659b4ffb74c84d0be2ddf28f3
kubrakhmd <kubraxe-bp217@code.edu.az>
2024-10-15 10:22:36 +0400

    15.10 added

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace task_15._10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           // string SentenceInput = ("    EverytHing i neeD, IS a CuP of CofFee   ");
+           // string SentenceOutput = (SentenceInput);
+           // Console.WriteLine(ModifiedSentence(SentenceOutput));
+            Product product = new Product("Apple", "Iphone13", 1000m, 700m, 10);
+            product.GetInfo();
+            product.Sale(5);
+            product.GetInfo();
+        }
+        public static string ModifiedSentence(string sentence)
+        {
+            sentence = sentence.Trim();
+            TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+            StringBuilder result = new StringBuilder();
+
+            string[] words = sentence.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (i > 0)
+                {
+                    result.Append(" "); 
+                }
+                result.Append(textInfo.ToTitleCase(words[i].ToLower()));
+            }
+
+            return result.ToString();
+
+        }
+    }
+}
---
+++ product.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_15._10
+{
+    internal class Product
+    {
+
+
+        public string Id;
+        public string BrandName;
+        public string Model;
+        public decimal Price;
+        public decimal Cost;
+        public decimal Income;
+        public int Count;
+
+        public Product(string brandName, string model, decimal price, decimal cost, int count)
+        {
+            Id = brandName.Substring(0, 2) + model.Substring(0, 2);
+            BrandName = brandName;
+            Model = model;
+            Price = price;
+            Cost = cost;
+            Income = 0;
+            if (count < 0)
+            {
+                count = 0;
+            }
+            else
+            {
+                Count = count;
+            }
+        }
+
+            public void GetInfo()
+            {
+                Console.WriteLine($"Id: {Id}, BrandName: {BrandName}, Model: {Model}, Price: {Price}, Cost: {Cost}, Income: {Income}, Count: {Count}");
+            
+        }
+
+            public void Sale(int quantity)
+            {
+                if (quantity > Count)
+                {
+                    Console.WriteLine("Kifayət qədər məhsul yoxdur.");
+
+                }
+                else
+                {
+                    decimal revenue = (Price - Cost) * quantity;
+                    Income += revenue;
+                    Count -= quantity;
+                }
+            }
+        }
+
+
+    } 
+
